869eba6b6d23ed9a8e6e7e854e54d0f2cd060cf8
Kenoboss <tziegelmayer@web.de>
2017-06-13 17:08:16 +0200

    insert little dialog

--- lib/lita/handlers/src/anfragen.rb
+++ lib/lita/handlers/src/anfragen.rb
@@ -1,11 +1,39 @@
-
 def get_start_stop (input)
   result = input.scan(/.*(\w[\- ]*Gebäude|Mensa|[Bb]ib[liothek]*|Raum \w\d+|[A-Z]\d+|\d+[\. ] Stockwerk).*(\w[\- ]*Gebäude|Mensa|[Bb]ib[liothek]*|Raum \w\d+|[A-Z]\d+|\d+[\. ] Stockwerk)/i)
 
   if result.empty? == false
     return result[0]
   else
-      return "Bitte formulieren Sie ihre Anfrage neu."
+      greeting = return_greeting(input)
+      if greeting.empty? == false
+        return greeting
+      else
+        return "Bitte formulieren Sie ihre Anfrage neu."
+      end
     end
+end
+
+
+def return_greeting (input)
+  input = input.strip()
+  greetings = {
+    "Hallo" => [ "Hi", "Guten Tag!!" ],
+    "Hi" => [ "Hallo" ],
+    "Guten Morgen!" => [ "Hallo","Danke dir","Guten Morgen!." ],
+    "Hi, wie geht es dir?" => [ "Mir geht es gut","Ganz okay","Super","Großartig","Könnte besser sein","Nicht so gut" ],
+    "Wie geht es dir?" => [ "Gut","Ganz gut, danke","Gut, und dir?" ],
+    "Schön dich zu sehen" => [ "Dankeschön." ],
+    "Wie gehts?" => [ "Geht ganz gut","Mir gehts gut. Wie geht es dir?" ],
+    "Hi, schön dich zu treffen" => [ "Danke, gleichfalls." ],
+    "Es ist eine Freude dich zu sehen" => [ "Dankeschön, freut mich auch" ],
+    "Was geht?" => [ "Nicht so viel","Nicht wirklich viel","Nicht viel, und bei dir?","Nichts","Die Sonne scheint und mir geht es gut. Wie sieht es mit dir aus?" ]
+  }
+  greeting = greetings[input]
+
+  begin
+    return greeting[rand(0..greeting.length-1)]
+  rescue
+    return []
+  end
 
 end
--- lib/lita/handlers/test/anfragen_test.rb
+++ lib/lita/handlers/test/anfragen_test.rb
@@ -5,8 +5,16 @@ class TestAnfragen < Test::Unit::TestCase
 
   def test_anfragen
     assert_equal( ["Bib","Mensa"], get_start_stop("Ich will von der Bib zur Mensa") )
+
     assert_equal( ["Bib","Mensa"], get_start_stop("Wie komme ich von der Bib zur Mensa.") )
+
     assert_equal( ["Mensa","Bib"], get_start_stop("Wie komme ich von der Mensa zur Bib.") )
+
+    assert_equal( ["A337","B329"], get_start_stop("Zeig mir den Weg von A337 zum Raum B329.") )
+
+    #assert_equal( ["A337","DM-Gebäude"], get_start_stop("Zeig mir den Weg von A337 zum DM-Gebäude.") )
+
+    assert_equal( "Hi", get_start_stop("Hallo ") )
   end
 
 end
